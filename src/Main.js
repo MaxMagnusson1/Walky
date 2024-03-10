0597745b66558fd03f74697cc52485d71e0721ed
MaxMagnusson1 <mm224zp@student.lnu.se>
2024-03-10 15:56:23 +0000

    te

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,15 @@
 var Main = {
     init: function () {
         // Visa laddningsikonen när sidan laddas
-        var loadingIcon = document.getElementById("loading-icon");
-        loadingIcon.style.display = "block";
-        var loadingControll = document.querySelectorAll("*"); 
+      //  var loadingIcon = document.getElementById("loading-icon");
+      //  loadingIcon.style.display = "block";
+       // var loadingControll = document.querySelectorAll("*"); 
         // Dölj laddningsikonen när allt innehåll är redo att visas
-            if (loadingControll.length === 22){
-                loadingIcon.style.display = "none";
+           // if (loadingControll.length === 22){
+              //  loadingIcon.style.display = "none";
                 var start = new Start();
                 start.renderMapAndButtons();
-            }
+       //     }
             // Dölj laddningsikonen när DOM:en har laddats
   
         }
