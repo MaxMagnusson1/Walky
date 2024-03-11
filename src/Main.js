ecc2d8cd9e7d127ec756b098396e80cfd7fa5e29
MaxMagnusson1 <mm224zp@student.lnu.se>
2024-03-11 16:31:35 +0100

    test

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,31 @@
 var Main = {
     init: function () {
-    
-                var start = new Start();
-                start.renderMapAndButtons();
-         }
-            // Dölj laddningsikonen när DOM:en har laddats
-  
-        }
-    
+        var start = new Start();
+        start.renderMapAndButtons();
+    }
+};
 
-        window.onload = function() {
-          Main.init();
-      };
+// Lyssna efter DOMContentLoaded-händelsen
+document.addEventListener("DOMContentLoaded", function() {
+    // Skapa en ny bild
+    var img = new Image();
+
+    // Ange källan till din bild
+    img.src = './img/paket.png';
+
+    // Lägg till en onload-händelse för att kalla på Main.init() när bilden har laddats
+    img.onload = function() {
+        Main.init();
+    };
+
+    // Om du har fler bilder, upprepa processen för varje bild
+
+    // Lägg till bilden till DOM:en om du vill att den ska visas på sidan
+    // document.body.appendChild(img);
+});
+
+// Lyssna efter window.onload-händelsen
+window.onload = function() {
+    // Hela sidan, inklusive bilder, har laddats helt
+    console.log("Hela sidan har laddats helt.");
+};
