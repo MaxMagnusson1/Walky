648091ccec5148aef2678ff563e6b6d6bf7855bd
MaxMagnusson1 <mm224zp@student.lnu.se>
2024-03-19 16:21:19 +0100

    klart

--- src/Main.js
+++ src/Main.js
@@ -13,12 +13,12 @@ Skapar en laddningsicon som visas när applikationen laddas in i väntan på att
 laddats in körs en onload som väntar på att alla bilder laddats in och när det är klart tas laddningsiconen bort och Main.init() körs
 vilket sätter igång star filen.
  */
-var loadingApp = document.createElement("div");
-loadingApp.className = "loadingApp";   
-document.body.appendChild(loadingApp); 
+
 
 document.addEventListener("DOMContentLoaded", function() {
-  
+    var loadingApp = document.createElement("div");
+    loadingApp.className = "loadingApp";   
+    document.body.appendChild(loadingApp); 
 
     var img = new Image();
     img.src = './img/correktpaket-05.png', './img/errorfeedback-03.png', './img/litenlocker-09.png','./img/coin2-10.png','./img/mapAsset 27@2x.png','./img/bigline-07.png','./img/skuggat-06.png', './img/lockerImg.png', './img/felhanteringklar-04.png','./img/BBQ-12.png', './img/cake-12.png','./img/disco-12.png', './img/kompassen-12.png', './img/lasersabel-12.png', './img/lasersabellila-12.png', './img/redlasersabel-12.png', './img/mic-12.png', './img/munk-12-png', './img/star-12.png', './img/coin3-10.png';
